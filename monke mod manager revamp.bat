cbd1c28af160bde29757cc77a050547b2af97ddf
CURSEDVROFFICAL <96031292+THECURSEDDEV@users.noreply.github.com>
2025-07-21 17:55:35 -0700

    Update monke mod manager revamp.bat

--- monke mod manager revamp.bat
+++ monke mod manager revamp.bat
@@ -2,24 +2,58 @@
 echo MADE BY CURSEDDEV ON DISCORD!
 timeout /t 3 >nul
 cls
-set /p gtagpath=Enter the path of gorilla tag: 
-cd %gtagpath%
+
+set steamPath1="C:/Program Files (x86)/Steam/steamapps/common/Gorilla Tag"
+set steamPath2="D:/SteamLibrary/steamapps/common/Gorilla Tag"
+set steamPath3="C:/Program Files/Oculus/Software/Software/another-axiom-gorilla-tag"
+
+if exist %steamPath1% (
+    set gamePath=%steamPath1%
+) else if exist %steamPath2% (
+    set gamePath=%steamPath2%
+) else if exist %steamPath3% (
+    set gamePath=%steamPath3%
+) else (
+    color 0c
+    Echo Gorilla Tag directory not found.
+    set /p gamePath=Enter the path of gorilla tag:
+)
+
+cd %gamePath%
 cd Bepinex
 cd plugins
-echo Newtilla *new utilla* (1)
-echo WhoIsTalking *name tag mod* (2)
+echo Welcome to MonkeMods1.00b
+echo Whats new:
+echo removed newtilla (outdated and unsupported)
+echo updated WhoIsTalking!
+echo updated Gorilla Shirts!
+echo Adds working utilla!!
+echo Adds grate mod menu
+echo.
+echo.
+echo WhoIsTalking *name tag mod* (1)
+echo GorillaShirts *custom shirts and player models!* (2)
+echo Utilla *modded and custom gamemodes. Required by some mods* (3)
+echo Grate mod menu *A legal mod menu forked from bark. will automatically install utilla* (4)
+echo.
+echo.
 echo more will be added soon!
 set /p selectedmod=What would you like to install: 
 if "%selectedmod%"=="1" (
-   :: Download the mod
-   curl -L -s https://github.com/Loafiat/Newtilla/releases/download/1.1.0/Newtilla.dll --output Newtilla.dll
+   curl -L -s https://github.com/The-Graze/WhoIsTalking/releases/download/5.2.1/WhoIsTalking.dll --output WhoIsTalking.dll
    echo Mod added!
 )
 if "%selectedmod%"=="2" (
-   :: Download the mod
-   curl -L -s https://github.com/The-Graze/WhoIsTalking/releases/download/4.2.0.4/WhoIsTalking.dll --output WhoIsTalking.dll
+   curl -L -s https://github.com/developer9998/GorillaShirts/releases/download/v1.2.1/GorillaShirts.dll --output GorillaShirts.dll
    echo Mod added!
-) else (
-   echo Error, that's not an option. Please restart the app and enter something that exists
+)
+if "%selectedmod%"=="3" (
+   curl -L -s https://github.com/developer9998/Utilla/releases/download/1.6.18/Utilla.dll --output Utilla.dll
+   echo Mod added!
+)
+if "%selectedmod%"=="4" (
+   curl -L -s https://github.com/developer9998/Utilla/releases/download/1.6.18/Utilla.dll --output Utilla.dll
+   curl -L -s https://github.com/The-Graze/Grate/releases/download/1.7.3/Grate.dll --output Grate.dll
+   echo Mod added!
 )
 pause
